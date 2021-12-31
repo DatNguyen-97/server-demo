f5bf9703ab9bb2f066763485b8b591d2eca9bbdb
dat nguyen <datnguyentanpts@gmail.com>
2021-12-31 15:26:14 +0700

    update

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const server = http.createServer((req,res)=>{
     res.end();
 })
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
